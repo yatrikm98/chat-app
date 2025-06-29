3a8a8a2337419bf0279e2364cbd854fb140c64bc
mac <mac@macs-MacBook-Air.local>
2025-06-29 22:40:07 +0530

    changed index.js

--- api/index.js
+++ api/index.js
@@ -21,7 +21,6 @@ mongoose.connect(process.env.MONGO_URI).then(() => {
 // const __filename = fileURLToPath(import.meta.url);
 // const __dirname = path.dirname(__filename);
 
-const __dirname = path.resolve();
 
 app.use(
     express.static(
