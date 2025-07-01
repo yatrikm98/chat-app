4ca1f638cd1c692f43fa74004f36fdba6bbeb31e
Yatrik Mehta <yatrikmehta82@gmail.com>
2025-07-01 22:18:40 +0530

    changing backend files in backend

--- backend/index.js
+++ backend/index.js
@@ -9,7 +9,7 @@ const io = new Server(server, {
     cors: {
         origin: [
             'http://localhost:5173',
-            'https://chat-app-sand-two.vercel.app'
+            'https://chat-app-psi-eight-96.vercel.app'
         ],
     },
 });
--- backend/server.js
+++ backend/server.js
@@ -15,7 +15,7 @@ app.use(express.json())
 app.use(cors({
     origin: [
         "http://localhost:5173",
-        "https://chat-app-sand-two.vercel.app"
+        "https://chat-app-psi-eight-96.vercel.app"
     ],
     credentials: true, 
 }));
