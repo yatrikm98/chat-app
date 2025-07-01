fd45c15828da6dac7baf5b2fb0c8022015f50634
Yatrik Mehta <yatrikmehta82@gmail.com>
2025-07-01 21:50:09 +0530

    changing backend files

--- backend/index.js
+++ backend/index.js
@@ -9,7 +9,7 @@ const io = new Server(server, {
     cors: {
         origin: [
             'http://localhost:5173',
-            'https://chat-app-mu-two-98.vercel.app'
+            'https://chat-app-sand-two.vercel.app'
         ],
     },
 });
